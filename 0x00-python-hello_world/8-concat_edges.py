e648da858a07892b8b6311c22a88358478b24842
Muhwezi <michealphenh10@gmail.com>
2024-03-12 19:38:10 +0300

    Concatenates took me over 3 hours secret was in the +

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+
+print(str[39:-53] + str[107:-17] + str[0:6])
