88a6112748f6e0cf73bf0230e10b0871cacf97cd
Muhwezi <michealphenh10@gmail.com>
2024-04-16 13:10:38 +0300

    Readme file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!usr/bin/python3
-"""This module defines a method for line reading"""
+"""Defines a function for file-line reading"""
 
 
 def read_file(filename=""):
-    """Function just reads file and prints to STDOUT"""
-    with open(filename, mode="r", encoding="UTF-8") as file:
+    """prints the contents of file and prints to STDOUT"""
+    with open(filename, encoding="UTF-8") as file:
         print(file.read() , end="")
