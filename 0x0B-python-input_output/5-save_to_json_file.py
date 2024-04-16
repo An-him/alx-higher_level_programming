079440fabee21f5354e9af4f6518b70c8eefe35a
Muhwezi <michealphenh10@gmail.com>
2024-04-16 22:28:54 +0300

    Serializes OBJ to a textfile

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,8 @@
+import json
+"""Writes Json representation to a file"""
+
+
+def save_to_json_file(my_obj, filename):
+    """writes obj [JSON] to an open file"""
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(my_obj, file)
