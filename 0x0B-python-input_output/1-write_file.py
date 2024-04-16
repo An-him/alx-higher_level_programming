57ced46e3b745689f6a109698c38ccdcc6bd646d
Muhwezi <michealphenh10@gmail.com>
2024-04-16 15:40:19 +0300

    write to a file returns number of chars

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,7 @@
 
 
 def write_file(filename="", text=""):
+    """opens filename in write mode returns the chars"""
     with open(filename, mode="w", encoding="utf-8") as file:
         num = file.write(text)
         return num
