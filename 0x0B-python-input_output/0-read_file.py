8b8c15e9988dba1ddf26396a5556f4da736ccd97
Muhwezi <michealphenh10@gmail.com>
2024-04-16 13:05:48 +0300

    Readme file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """Function just reads file and prints to STDOUT"""
-    with open(filename, encoding="UTF-8") as f:
-        for line in f:
-            print(line, end="")
+    with open(filename, mode="r", encoding="UTF-8") as f:
+        content = f.read()
+        print(content)
