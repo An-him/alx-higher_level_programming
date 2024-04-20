be7ddac3a5ccf4486557697a71a1ae31bf5511b1
Muhwezi <michealphenh10@gmail.com>
2024-04-20 23:47:15 +0300

    New lingo isinstance

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,4 +9,4 @@ class Square:
             raise TypeError("size must be an integer")
         self.__size = size
         if size < 0:
-            raise ValueError("size must be <= 0")
+            raise ValueError("size must be >= 0")
