f85002d8727994666a6f9f52a92232551c767262
Muhwezi <michealphenh10@gmail.com>
2024-06-18 23:37:20 +0300

    printing 3 lines with newline

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+//script prints 3 lines of text with a newline
+console.log("C is fun")
+console.log("Python is cool")
+console.log("JavaScript is amazing")
