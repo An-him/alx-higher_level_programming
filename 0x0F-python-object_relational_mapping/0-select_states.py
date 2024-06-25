355c58ba7d55f67e1bae078409d6478966f6affa
Muhwezi <michealphenh10@gmail.com>
2024-06-25 12:36:38 +0300

    Inital commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import MySQLdb
+db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="3M-kfk.rzcw-t6k", db="hbtn_0e_0_usa")
+cur = db.cursor()
+cur.execute("SELECT * FROM states")
+rows = cur.fetchall()
+for row in rows:
+    print("{} ".format(row))
+cur.close()
+db.close()
