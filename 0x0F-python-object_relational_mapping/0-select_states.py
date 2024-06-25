7f46291a2c28587a643f3d4e10e36fa50d26053e
Muhwezi <michealphenh10@gmail.com>
2024-06-25 21:56:43 +0300

    usage of sys.argv and looping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,17 +3,16 @@
 """
 script for connection and display states
 """
+import MySQLdb
+from sys import argv
+
 if __name__ == "__main__":
 
-    import MySQLdb
-    import sys
-
-db = MySQLdb.connect(host="localhost", port=3306,
-                     user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print("{} ".format(row))
-cur.close()
-db.close()
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    for state in cur.fetchall():
+        print(state)
+    cur.close()
+    db.close()
