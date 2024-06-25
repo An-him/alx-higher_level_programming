3b31d6f24a711c1075ad298e15eb28cfad7fca09
Muhwezi <michealphenh10@gmail.com>
2024-06-25 13:47:26 +0300

    Order By states.id added

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-# script for connection and display states
 
-import MySQLdb
+"""
+# script for connection and display states
+"""
+if __name__ == "__main__":
+    import MySQLdb
 db = MySQLdb.connect(host="localhost", port=3306,
                      user="root", passwd="3M-kfk.rzcw-t6k",
                      db="hbtn_0e_0_usa")
 cur = db.cursor()
-cur.execute("SELECT * FROM states")
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 rows = cur.fetchall()
 for row in rows:
     print("{} ".format(row))
