aefdf4dd96e4eb5e98caa168635b008a0d1729fd
Muhwezi <michealphenh10@gmail.com>
2024-06-25 16:16:20 +0300

    Added sys.argv

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 """
-# script for connection and display states
+script for connection and display states
 """
 if __name__ == "__main__":
+
     import MySQLdb
+    import sys
+
 db = MySQLdb.connect(host="localhost", port=3306,
-                     user="root", passwd="3M-kfk.rzcw-t6k",
-                     db="hbtn_0e_0_usa")
+                     user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 cur = db.cursor()
 cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 rows = cur.fetchall()
