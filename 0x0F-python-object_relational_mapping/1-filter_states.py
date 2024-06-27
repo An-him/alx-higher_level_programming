7bfe69137640bed69931bdc24f07fd495421c582
Muhwezi <michealphenh10@gmail.com>
2024-06-27 19:34:27 +0300

    select states by first letter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT*FROM states WHERE\
- name LIKE 'N%' ORDER BY states.id ASC")
+    cur.execute("""SELECT*FROM states WHERE
+ name LIKE BINARY 'N%' ORDER BY states.id ASC""")
     for state in cur.fetchall():
         print(state)
     cur.close()
