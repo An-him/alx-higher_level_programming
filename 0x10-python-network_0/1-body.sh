eb01fe686e18bded0df3f653c0f7bcf18308a02d
Muhwezi <michealphenh10@gmail.com>
2024-08-01 03:54:11 +0300

    COmmit to 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and displays the response
-curl -sL "$1"
+curl -s -w "%{http_code}" -o temp_response_body "$1" | grep -q "200$" && cat temp_response_body && rm -f temp_response_body
