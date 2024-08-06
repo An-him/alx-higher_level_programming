45fa9cc8445ae700247a65d3b5a3a38f0c851144
Muhwezi <michealphenh10@gmail.com>
2024-08-06 14:20:43 +0300

    1 body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# takes in a URL, sends a GET request to the URL, and displays the response
-curl -s -w "%{http_code}" -o temp_response_body "$1" | grep -q "200$" && cat temp_response_body && rm -f temp_response_body
+curl -sL "$1"
