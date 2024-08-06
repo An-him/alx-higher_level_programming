622b81516e6fe0b0bb4c85e6e41f801bb4a656fe
Muhwezi <michealphenh10@gmail.com>
2024-08-06 20:27:33 +0300

    101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Send the POST request and display the response body
-curl -X POST -H "Content-Type: application/json" -d @"$2" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
