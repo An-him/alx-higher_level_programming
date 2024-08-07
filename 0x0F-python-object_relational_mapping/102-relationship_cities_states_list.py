00adf5822cff71c8f99870cf3cdc41dece7e6761
Muhwezi <michealphenh10@gmail.com>
2024-08-08 02:34:57 +0300

    Pycodestyle

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
 """
-a script that changes the name of a State object from the database
-"""
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import sys
 from relationship_state import Base, State
 from relationship_city import City
-import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
 
 if __name__ == "__main__":
-    """connect to the server """
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.
-        format(sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
-
-    """ Create session"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    """ Query with relationship """
-    cities = session.query(City).order_by(City.id).all()
-
-    for city in cities:
-        print(f'{city.id}: {city.name} -> {city.state.name}')
+    for instance in session.query(State).order_by(State.id):
+        for city_ins in instance.cities:
+            print(city_ins.id, city_ins.name, sep=": ", end="")
+            print(" -> " + instance.name)
