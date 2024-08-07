572567af4d9a8142838dbac0087b5f282cd1ac7e
Muhwezi <michealphenh10@gmail.com>
2024-08-08 02:37:10 +0300

    Pycodestyle

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-"""
-a script that changes the name of a State object from the database
-"""
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+"""List all states"""
+from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
-import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """connect to the server """
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.
-        format(sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
 
-    """ Create session"""
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    """ Query with relationship """
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
-        print(f'{state.id}: {state.name}')
-
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print(f'    {city.id}: {city.name}')
+            print("\t{}: {}".format(city.id, city.name))
