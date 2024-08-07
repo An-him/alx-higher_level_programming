277b6ed10fb7b2bffad20b2e38aa73ff4e5bb852
Muhwezi <michealphenh10@gmail.com>
2024-08-08 02:02:53 +0300

    4 by state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-"""Lists states"""
-
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
-                "JOIN states ON cities.state_id = states.id "
-                "ORDER BY cities.id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    # Get MySQL credentials and database name from command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the query to retrieve all cities sorted by cities.id
+    cursor.execute("SELECT cities.id, cities.name FROM cities ORDER BY cities.id ASC")
+
+    # Fetch all the results
+    cities = cursor.fetchall()
+
+    # Print the results
+    for city in cities:
+        print(city)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
